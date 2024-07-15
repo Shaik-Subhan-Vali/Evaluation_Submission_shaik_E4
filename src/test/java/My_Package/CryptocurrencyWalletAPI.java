aded4aab4bf344ac93a3f16ebe70efb818544789
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-15 15:55:10 +0530

    Tests updated

--- src/test/java/My_Package/CryptocurrencyWalletAPI.java
+++ src/test/java/My_Package/CryptocurrencyWalletAPI.java
@@ -13,12 +13,13 @@ public class CryptocurrencyWalletAPI {
 	
 	@Test
 	public void Register_user() {
+		//created a json obj to store request paylod
 		JSONObject json = new JSONObject();
 		json.put("username", "user123");
 		json.put("password", "securepassword");
 		json.put("email", "user@example.com");
 		Response Res = given().contentType(ContentType.JSON).body(json.toJSONString()).when().post("https://crypto-wallet-server.mock.beeceptor.com/api/v1/register");
-			
+		//validating the response payload	
 		Res.then().statusCode(200).log().all();
 		Res.jsonPath().getString("username").equals("user123");
 		Res.jsonPath().getString("password").equals("securepassword");
@@ -33,6 +34,7 @@ public class CryptocurrencyWalletAPI {
 		json.put("password", "securepassword");
 		Response Res = given().contentType(ContentType.JSON).body(json.toJSONString()).when().post("https://crypto-wallet-server.mock.beeceptor.com/api/v1/login");
 		
+		//created a token vraible to store generated token
 		token = Res.jsonPath().getString("access_token");
 		 
 		Res
@@ -44,6 +46,7 @@ public class CryptocurrencyWalletAPI {
 	
 	@Test
 	public void Retrieve_wallet_balance() {
+		//using get method to retrieve response payload
 		Response Res = given().get("https://crypto-wallet-server.mock.beeceptor.com/api/v1/balance");
 		Res.then().statusCode(200).log().all();
 		Res.jsonPath().getString("balance").equals("100.25");
@@ -52,9 +55,10 @@ public class CryptocurrencyWalletAPI {
 	}
 	@Test
 	public void List_transactions() {
+		//using get method to retrieve response payload
 		Response Res = given().get("https://crypto-wallet-server.mock.beeceptor.com/api/v1/transactions");
 		Res.then().statusCode(200).log().all();
-		
+		//validating the response payload	
 		Res.jsonPath().getString("transactions[0].id").equals("12345");
 		Res.jsonPath().getString("transactions[0].amount").equals("10.5");
 		
@@ -67,7 +71,7 @@ public class CryptocurrencyWalletAPI {
 		json.put("amount", "5.0");
 		json.put("currency", "ETH");
 		Response Res = given().contentType(ContentType.JSON).body(json.toJSONString()).when().post("https://crypto-wallet-server.mock.beeceptor.com/api/v1/transactions");
-			
+		//validating the response payload		
 		Res.then().statusCode(200).log().all();
 		Res.jsonPath().getString("id").equals("98765");
 		Res.jsonPath().getString("currency").equals("ETH");
@@ -80,7 +84,7 @@ public class CryptocurrencyWalletAPI {
 		json.put("amount", "2.5");
 		json.put("currency", "BTC");
 		Response Res = given().contentType(ContentType.JSON).body(json.toJSONString()).when().post("https://crypto-wallet-server.mock.beeceptor.com/api/v1/transaction_fee");
-			
+		//validating the response payload		
 		Res.then().statusCode(200).log().all();
 		Res.jsonPath().getString("fee").equals("0.0005");
 		Res.jsonPath().getString("currency").equals("BTC");
