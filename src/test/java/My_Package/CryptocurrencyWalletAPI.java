ee8fd1dc81744675a8264329c808d24d223daee1
Shaik-Subhan-Vali <subhanshaik7781@gmail.com>
2024-07-15 15:33:11 +0530

    Tests added

--- src/test/java/My_Package/CryptocurrencyWalletAPI.java
+++ src/test/java/My_Package/CryptocurrencyWalletAPI.java
@@ -1,5 +1,47 @@
 package My_Package;
 
-public class CryptocurrencyWalletAPI {
+import static io.restassured.RestAssured.given;
 
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class CryptocurrencyWalletAPI {
+	String token ;
+	
+	@Test
+	public void Register_user() {
+		JSONObject json = new JSONObject();
+		json.put("username", "user123");
+		json.put("password", "securepassword");
+		json.put("email", "user@example.com");
+		Response Res = given().contentType(ContentType.JSON).body(json.toJSONString()).when().post("https://crypto-wallet-server.mock.beeceptor.com/api/v1/register");
+			
+		Res.then().statusCode(200).log().all();
+		Res.jsonPath().getString("username").equals("user123");
+		Res.jsonPath().getString("password").equals("securepassword");
+		
+			
+	}
+	
+	@Test
+	public void Login_user() {
+		JSONObject json = new JSONObject();
+		json.put("username", "user123");
+		json.put("password", "securepassword");
+		Response Res = given().contentType(ContentType.JSON).body(json.toJSONString()).when().post("https://crypto-wallet-server.mock.beeceptor.com/api/v1/login");
+		
+		token = Res.jsonPath().getString("access_token");
+		 
+		Res
+		.then().statusCode(200).log().all();
+		
+		Res.jsonPath().getString("token_type").equals("bearer");
+	
+		
+	}
+
+	
 }
